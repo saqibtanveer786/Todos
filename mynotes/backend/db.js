a09d15c40ce1001bc6739a5f2a89dd16b2544ada
Saqib Tanveer <adward797@gmail.com>
2023-02-12 02:24:40 -0800

    commit3

--- mynotes/backend/db.js
+++ mynotes/backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
  
 const server = '127.0.0.1:27017'
-const database = 'test'
+const database = 'Test'
 
 const connectToMongo = async()=>{
    await mongoose.connect(`mongodb://${server}/${database}`)
--- mynotes/backend/routes/auth.js
+++ mynotes/backend/routes/auth.js
@@ -1,8 +1,12 @@
-const mongoose = require('mongoose')
+
 const express = require('express')
 const router = express.Router()
 const User = require('../modals/Users')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const secret = 'secret'
 
 // this end point is to register user: "Post"  Atuntication not require Login also
 router.post('/signup',
@@ -10,36 +14,95 @@ router.post('/signup',
   body('name').isLength({ min: 3 }),
   body('password').isLength({ min: 5 }), 
 async (req, res) => {
+
+  //Getting user information from request: Using destructuring
+  const {name, email, password} = req.body
+
+  //Checking Errors : if there are errors then return bad request.
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  }).then(user => res.json(user));
+
+  //checking whether user with this already exists or not.
+  const checkingUsers = await User.findOne({email})
+  if (checkingUsers) {
+    return res.status(500).send('Please try right information')
+  }
+
+  //Registring new user: if user doesn't exists with given information.
+  try {
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  let user = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  })
+  const data = {
+    user:{
+      id: user._id
+    }
+  }
+  //Sending authtoken.
+  const authtoken = jwt.sign(data, secret )
+  res.send(authtoken)
+  //If any internal error loging on console.
+  } catch (error) {
+    console.log('Some internal error')
+  }
 })
 
-// this end point is to login user: "Post"  Atuntication not require Login also
+
+
+
+
+                            // Route:2    Login
+
+// Route:2 This end point is to login user: "Post"  Loin not require
 router.post('/login',
   body('email').isEmail(),
   body('password').isLength({ min: 5 }), 
 async (req, res) => {
+
+  //Getting user information from request: Using destructuring
+  const {email, password} = req.body
+
   let user
+
+  //Checking Errors : if there are errors then return bad request.
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
+
+  //Login functionality
   try {
+    //check user exists or not
+    user = await User.findOne({email})
+    if (!user) {
+      return res.status(500).send('Please try right information')
+    }
     
-    user = await User.findById("63e7ca088e641105f666d74c")
-  } catch (error) {
-    res.status(400).send('enter an id')
-  }
-  if (user) {
-    res.send(user)
-  }
+    //checking password is correct or not.
+    let checkpassword = await bcrypt.compare(password, user.password)
+    if(!checkpassword){
+      return res.status(500).send('Please try right information')
+    }
+
+    //this variable is for authtoken
+    const data = {
+      user:{
+        id: user._id
+      }
+    }
+    //Sending authtoken.
+    const authtoken = jwt.sign(data, secret )
+    res.send(authtoken)
+    //If any internal error loging on console.
+    } catch (error) {
+      console.log('Some internal error')
+    }
 })
 
 
